f150fb05becfc594770d4a1a61eca1ccccabca27
Manuel Napoli <mnapoli@192.168.1.20>
2021-04-26 11:29:39 -0300

    Backend fixed

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   required_version = ">= 0.14.9"
-}
-backend "s3" {
-  bucket = "a9d6a632-e7b1-aa8d-6f04-1d95ca2ab1ef-backend"
-  key    = "terraform/webpage/terraform.tfstate"
-  region = "us-east-1"
+  backend "s3" {
+    bucket = "a9d6a632-e7b1-aa8d-6f04-1d95ca2ab1ef-backend"
+    key    = "terraform/webpage/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
